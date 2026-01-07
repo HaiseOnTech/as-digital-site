0c8bf4230d386a6b344061af5049887af1e72fc8
Sélim Aïdi <selimaidi@mac.home>
2026-01-07 17:25:02 +0100

    fix: correct site URL in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://asdigitalsolutions.pro',
+  site: 'https://selimaidi.com',
   integrations: [
     sitemap()
   ],
